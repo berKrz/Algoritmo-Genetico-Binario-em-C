0548dfb1af9de6881c33e01c888d789b6afb9d6e
berKrz <bernardo@krzyuy.com.br>
2026-08-26 18:55:18 -0300

    feat: generation loop

--- main.c
+++ main.c
@@ -13,10 +13,13 @@ int main() {
   init(pop);
   wait_for_enter();
 
-  roleta(pop);
-  wait_for_enter();
+  for (int i = 0; i < 10; i++) {
+    printf("\n\nIteração %d\n", i);
+    roleta(pop);
+    wait_for_enter();
 
-  crossover_pop(pop);
+    crossover_pop(pop);
+  }
 
   return EXIT_SUCCESS;
 }
